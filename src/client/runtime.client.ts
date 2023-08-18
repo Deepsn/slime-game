a5e492032a2da05c8bc0ac3d8d7d37405d942899
Deepsn <75620590+Deepsn@users.noreply.github.com>
2023-08-18 11:22:38 -0300

    feat: add zircon console pkg: remove lapis

--- src/client/runtime.client.ts
+++ src/client/runtime.client.ts
@@ -1,7 +1,22 @@
 import { Flamework, Modding } from "@flamework/core";
 import Log, { Logger } from "@rbxts/log";
+import Zircon, { ZirconClient } from "@rbxts/zircon";
 
-Log.SetLogger(Logger.configure().WriteTo(Log.RobloxOutput()).Create());
+Log.SetLogger(
+	Logger.configure()
+		.WriteTo(Log.RobloxOutput())
+		.WriteTo(Zircon.Log.Console())
+		.EnrichWithProperty("Version", "1.0.0")
+		.Create(),
+);
+
+task.wait(1);
+
+ZirconClient.Init({
+	AutoFocusTextBox: false,
+	EnableTags: true,
+	Keys: [Enum.KeyCode.Insert],
+});
 
 Modding.registerDependency<Logger>((actor) => Log.ForContext(actor));
 
--- src/server/runtime.server.ts
+++ src/server/runtime.server.ts
@@ -1,7 +1,16 @@
 import { Flamework, Modding } from "@flamework/core";
 import Log, { Logger } from "@rbxts/log";
+import Zircon, { ZirconConfigurationBuilder, ZirconServer } from "@rbxts/zircon";
 
-Log.SetLogger(Logger.configure().WriteTo(Log.RobloxOutput()).Create());
+Log.SetLogger(
+	Logger.configure()
+		.WriteTo(Log.RobloxOutput())
+		.WriteTo(Zircon.Log.Console())
+		.EnrichWithProperty("Version", "1.0.0")
+		.Create(),
+);
+
+ZirconServer.Registry.Init(ZirconConfigurationBuilder.default().Build());
 
 Modding.registerDependency<Logger>((actor) => Log.ForContext(actor));
 
