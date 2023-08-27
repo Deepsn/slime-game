ca292a662c52739306cd35adaf57bdb6535dcefb
Deepsn <75620590+Deepsn@users.noreply.github.com>
2023-08-27 11:58:30 -0300

    feat: boost fuel

--- src/client/controllers/BoostController.ts
+++ src/client/controllers/BoostController.ts
@@ -9,24 +9,31 @@ function lerp(a: number, b: number, t: number) {
 @Controller()
 export class BoostController implements OnStart, OnRender {
 	public enabled = false;
+	public fuel = 0;
+
+	private readonly BASE_FUEL = 100;
 	private readonly CAMERA_SPEED = 10;
+	private holding = false;
+	private lastEnabled = tick();
 
 	constructor(private readonly cameraController: CameraController) {}
 
 	onStart(): void {
+		this.fuel = this.BASE_FUEL;
+
 		UserInputService.InputBegan.Connect((input, gameProcessed) => {
 			if (gameProcessed) {
 				return;
 			}
 
 			if (input.KeyCode === Enum.KeyCode.E) {
-				this.enabled = true;
+				this.holding = true;
 			}
 		});
 
 		UserInputService.InputEnded.Connect((input) => {
 			if (input.KeyCode === Enum.KeyCode.E) {
-				this.enabled = false;
+				this.holding = false;
 			}
 		});
 	}
@@ -35,5 +42,19 @@ export class BoostController implements OnStart, OnRender {
 		const camera = this.cameraController.camera;
 
 		camera.FieldOfView = lerp(camera.FieldOfView, this.enabled ? 80 : 70, dt * this.CAMERA_SPEED);
+
+		if (this.holding && this.fuel > 0) {
+			this.fuel -= dt * 10;
+			this.enabled = true;
+		} else {
+			if (!this.holding && tick() - this.lastEnabled > 1) {
+				this.fuel += dt;
+			}
+
+			if (this.enabled) {
+				this.enabled = false;
+				this.lastEnabled = tick();
+			}
+		}
 	}
 }
