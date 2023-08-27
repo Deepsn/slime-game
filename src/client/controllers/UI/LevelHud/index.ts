2ae29908c03db20144f0a468646a2b4956fb1aab
Deepsn <75620590+Deepsn@users.noreply.github.com>
2023-08-27 14:16:57 -0300

    feat: show on ui next world level

--- src/client/controllers/UI/LevelHud/index.ts
+++ src/client/controllers/UI/LevelHud/index.ts
@@ -2,8 +2,10 @@ import { Controller } from "@flamework/core";
 import { UIClass } from "../UIClass";
 import { LevelHudFrame } from "./types";
 import { producer } from "client/producers";
-import { selectPlayerStats } from "shared/selectors";
+import { selectPlayerStats, selectPlayerWorlds } from "shared/selectors";
 import { TweenService } from "@rbxts/services";
+import { createSelector } from "@rbxts/reflex";
+import { worldsLevel } from "shared/lib/worlds";
 
 @Controller()
 export class LevelHud extends UIClass<LevelHudFrame> {
@@ -12,6 +14,12 @@ export class LevelHud extends UIClass<LevelHudFrame> {
 	}
 
 	onStart() {
+		const selectPlayerCurrentWorld = (playerId: string) => {
+			return createSelector(selectPlayerWorlds(playerId), (worlds) => {
+				return worlds?.selected;
+			});
+		};
+
 		producer.subscribe(selectPlayerStats(tostring(this.localPlayer.UserId)), (stats) => {
 			if (!stats) {
 				return;
@@ -22,6 +30,28 @@ export class LevelHud extends UIClass<LevelHudFrame> {
 			}).Play();
 			this.instance.Level.LevelGUI.Point.Text = `${stats.experience}/${stats.maxExperience}`;
 			this.instance.Level.LevelGUI.LevelNum.Text = `${stats.level}`;
+		});
+
+		producer.subscribe(selectPlayerCurrentWorld(tostring(this.localPlayer.UserId)), (area) => {
+			if (area === undefined) {
+				return;
+			}
+
+			const currentWorldId = tonumber(area.match("%d")[0]);
+
+			if (currentWorldId === undefined) {
+				return;
+			}
+
+			const nextWorldLevel = worldsLevel[`Area${currentWorldId + 1}`];
+
+			if (nextWorldLevel === undefined) {
+				this.instance.Level.Title.Visible = false;
+				return;
+			}
+
+			this.instance.Level.Title.Visible = true;
+			this.instance.Level.Title.Text = `New world at level ${nextWorldLevel}`;
 		});
 	}
 }
